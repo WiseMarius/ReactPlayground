40ee6439db094757af25063c09c3dc7062d4a147
Marius Demeter <marius.demeter96@yahoo.com>
2023-05-18 08:27:54 +0300

    Added ExpensesFilter Component

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,16 +1,29 @@
+import ExpensesFilter from "./ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
+import { useState } from "react";
 
 function Expenses(props) {
+  const [selectedYear, setSelectedYear] = useState();
+
+  const onYearChangedHandler = (event) => {
+    setSelectedYear(event.target.value);
+  }
+
+  console.log(selectedYear)
+
   return (
-    <div className="expenses">
-      {props.expenses.map((expense) => (
-        <ExpenseItem
-          key={Math.random()}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+    <div>
+      <ExpensesFilter onYearChanged={onYearChangedHandler}/>
+      <div className="expenses">
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            key={Math.random()}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </div>
     </div>
   );
 }
