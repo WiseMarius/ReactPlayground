fc8a39024e5812f9787a6bcb2db60b0c11bc23b5
Marius Demeter <marius.demeter96@yahoo.com>
2023-05-23 21:44:58 +0300

    Added name validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,14 +1,50 @@
 import React from "react";
+import { useState } from "react";
 
 const SimpleInput = (props) => {
+  const [name, setName] = useState("");
+  const [nameWasTouched, setNameWasTouched] = useState(false);
+
+  const nameIsValid = name.trim() !== "" && nameWasTouched;
+  const formIsValid = nameIsValid;
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    setName("");
+    setNameWasTouched(false);
+  };
+
+  const onNameTouchedHandler = () => {
+    setNameWasTouched(true);
+  };
+
+  const onNameChangeHandler = (event) => {
+    setName(event.target.value);
+  };
+
+  const nameInputClass =
+    !nameIsValid && nameWasTouched ? "form-control invalid" : "form-control";
+
   return (
     <form>
-      <div className="form-control">
+      <div className={nameInputClass}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input
+          type="text"
+          id="name"
+          onChange={onNameChangeHandler}
+          onBlur={onNameTouchedHandler}
+          value={name}
+        />
+        {!nameIsValid && nameWasTouched && (
+          <p className="error-text">Name is not valid!</p>
+        )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid} onClick={onSubmitHandler}>
+          Submit
+        </button>
       </div>
     </form>
   );
